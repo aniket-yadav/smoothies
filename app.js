3233e71ca94a882a877232bec52a528cc847f3ce
aniket yadav <aniket4897@gmail.com>
2020-09-22 14:49:17 +0530

    make it better

--- app.js
+++ app.js
@@ -15,9 +15,10 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 // database connection
+const PORT = process.env.PORT;
 const dbURI = "mongodb+srv://Aniket:aniket1234@nodetutorials.ey0dd.mongodb.net/node-awt?retryWrites=true&w=majority";
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
+  .then((result) => app.listen(PORT))
   .catch((err) => console.log(err));
 
 // routes
