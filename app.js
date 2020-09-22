2b7ad8aef0509ef566ec2025e7b4e96646c85c70
Aniket Yadav <aniket4897@gmail.com>
2020-09-22 14:36:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = "mongodb+srv://Aniket:aniket1234@nodetutorials.ey0dd.mongodb.net/node-awt?retryWrites=true&w=majority";
+const dbURI = "mongodb+srv://<USER>:<PASSWORD>@<CLUSTER>.ey0dd.mongodb.net/<DBNAME>?retryWrites=true&w=majority";
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
